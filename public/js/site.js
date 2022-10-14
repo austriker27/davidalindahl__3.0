3c634932ed8d5e32653a0c01803139390c36f147
austriker27 <davidalindahl@gmail.com>
2022-10-14 10:23:43 -0700

    Public js and css changes

--- public/js/site.js
+++ public/js/site.js
@@ -2992,7 +2992,9 @@ __webpack_require__.r(__webpack_exports__);
 
 window.Alpine = alpinejs__WEBPACK_IMPORTED_MODULE_0__["default"];
 alpinejs__WEBPACK_IMPORTED_MODULE_0__["default"].start();
-sal_js__WEBPACK_IMPORTED_MODULE_1___default()();
+sal_js__WEBPACK_IMPORTED_MODULE_1___default()({
+  threshold: 0.33
+});
 
 /***/ }),
 
